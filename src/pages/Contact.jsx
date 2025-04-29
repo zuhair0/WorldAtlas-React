c4f35b039e32677e87a1c8600b760952e0882245
HP <zuhairsaleem343@gmail.com>
2025-04-29 19:37:10 +0500

    contact form submit function is created

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,13 @@
 export const Contact = () => {
+  const handleFormSubmit = (formData) => {
+    const  formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
+  }
   return (
     <section className="section-contact">
       <h2 className="container-title">Contact Us</h2>
       <div className="contact-wrapper container">
-        <form action="">
+        <form action={handleFormSubmit}>
           <input
             type="text"
             className="form-control"

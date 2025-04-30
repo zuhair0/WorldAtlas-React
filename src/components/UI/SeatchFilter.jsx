ad24ac63b7c96b16e38a402f4371904fa8e4b714
HP <zuhairsaleem343@gmail.com>
2025-04-30 21:15:06 +0500

    asc and desc functionality is completed

--- src/components/UI/SeatchFilter.jsx
+++ src/components/UI/SeatchFilter.jsx
@@ -1,25 +1,51 @@
-export const SeatchFilter = ({ search, setSearch, filter, setFilter }) => {
-    const handleInputValue = (e) => {
-        e.preventDefault()
-        setSearch(e.target.value);
-    };
-    const handleSeleteChange = (e) => {
-        e.preventDefault()
-        setFilter(e.target.value);
-    }
-    return (
-        <section className="section-searchFilter container"> 
-            <input type="text" placeholder="search" value={search} onChange={handleInputValue}/>
-            <div>
-                <select className="select-section" value={filter} onChange={handleSeleteChange}>
-                    <option value="all">All</option>
-                    <option value="Africa">Africa</option>
-                    <option value="Americas">Americas</option>
-                    <option value="Asia">Asia</option>
-                    <option value="Europe">Europe</option>
-                    <option value="Oceania">Oceania</option>
-                </select>
-            </div>
-        </section>
-    )
-}
+export const SeatchFilter = ({
+  search,
+  setSearch,
+  filter,
+  setFilter,
+  countries,
+  setCountries,
+}) => {
+  const handleInputValue = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+  const handleSeleteChange = (e) => {
+    e.preventDefault();
+    setFilter(e.target.value);
+  };
+  const sortCountries = (value) => {
+    const sortCountry = [...countries].sort((a, b) => {
+      return value === "asc"
+        ? a.name.common.localeCompare(b.name.common)
+        : b.name.common.localeCompare(a.name.common);
+    });
+    setCountries(sortCountry);
+  };
+  return (
+    <section className="section-searchFilter container">
+      <input
+        type="text"
+        placeholder="search"
+        value={search}
+        onChange={handleInputValue}
+      />
+      <button onClick={() => sortCountries("asc")}>Asc</button>
+      <button onClick={() => sortCountries("desc")}>Desc</button>
+      <div>
+        <select
+          className="select-section"
+          value={filter}
+          onChange={handleSeleteChange}
+        >
+          <option value="all">All</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+    </section>
+  );
+};
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -5,43 +5,50 @@ import { CountryCard } from "../components/Layout/CountryCard";
 import { SeatchFilter } from "../components/UI/SeatchFilter";
 
 export const Country = () => {
-  const [isPending, startTransition] = useTransition()
-  const [countries, setCountries] = useState([])
-  const [search, setSearch] = useState()
-  const [filter, setFilter] = useState("all") //setting filter to 'all' to that we can see all the countries at first
-  useEffect(()=>{
-    startTransition(async() => {
-      const res = await getCountryData()
+  const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState();
+  const [filter, setFilter] = useState("all"); //setting filter to 'all' to that we can see all the countries at first
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
       console.log(res.data);
-      setCountries(res.data)
-
-    })
-  },[])
-  if(isPending) return <Loader />
+      setCountries(res.data);
+    });
+  }, []);
+  if (isPending) return <Loader />;
 
   // logic for filtering the countries based on the search input and filter selection
   const searchCountry = (country) => {
-    if(search){
-      return country.name.common.toLowerCase().includes(search.toLowerCase())
+    if (search) {
+      return country.name.common.toLowerCase().includes(search.toLowerCase());
     }
     return country;
-  }
-  const filterRegion = (country) =>{
-    if(filter === "all")return country;
-    return country.region === filter
-  }
-  const filteredCountries = countries.filter((country)=> searchCountry(country) && filterRegion(country))
-  return (<>
-  <section className="country-section">
-    <SeatchFilter search={search} setSearch={setSearch} filter={filter} setFilter={setFilter}/>
-    <ul className="grid grid-four-cols">
-      {
-        filteredCountries.map((curCountry, index)=>{
-          return <CountryCard country={curCountry} key={index}/>
-        })
-      }
-    </ul>
-  </section>
-  </>
+  };
+  const filterRegion = (country) => {
+    if (filter === "all") return country;
+    return country.region === filter;
+  };
+  const filteredCountries = countries.filter(
+    (country) => searchCountry(country) && filterRegion(country)
   );
-}
+  return (
+    <>
+      <section className="country-section">
+        <SeatchFilter
+          search={search}
+          setSearch={setSearch}
+          filter={filter}
+          setFilter={setFilter}
+          countries={countries}
+          setCountries={setCountries}
+        />
+        <ul className="grid grid-four-cols">
+          {filteredCountries.map((curCountry, index) => {
+            return <CountryCard country={curCountry} key={index} />;
+          })}
+        </ul>
+      </section>
+    </>
+  );
+};
